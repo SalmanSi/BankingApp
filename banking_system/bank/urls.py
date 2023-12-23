aee07ae70988b4665703c855d923097bb7e2e537
Salman Siddiq <salmansiddiq566@gmail.com>
2023-12-24 03:17:09 +0500

    added registration functionality
    added trigger to register an account whenever new user registers

--- banking_system/bank/urls.py
+++ banking_system/bank/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns=[
     path("",views.index,name="index"),
     path("login/",views.login_view,name="login"),
-    path("home/",views.home,name='home'),
+    path("user/",views.index,name='user'),
+    path("logout/",views.logout_view,name="logout"),
+    path("register/",views.register_view,name="register"),
 ]
--- banking_system/bank/views.py
+++ banking_system/bank/views.py
@@ -7,25 +7,49 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def index(request):
-
-    return render(request,"bank/index.html")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request,"bank/user.html")
     
 
 def login_view(request):
-    user=User(username="salman",password="salman")
+    
     if request.method=="POST":
         username=request.POST["username"]
         password=request.POST["password"]
         user=authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
-            return HttpResponseRedirect(reverse('home'))
+            return HttpResponseRedirect(reverse('index'))
         else:
             return render(request,"bank/login.html",{
                 "message":"Invalid credentials."
             })
-    return render(request,"bank/login.html")
+    return render(request,"bank/login.html")    
         
-    
-def home(request):
-    return render(request,"bank/home.html")
+def logout_view(request):
+    logout(request)
+    return render(request,"bank/login.html",{
+        "message":"Logged Out"
+    })
+
+def register_view(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"]
+        email=request.POST["email"]
+        # create user
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+        login(request,user)
+        return HttpResponseRedirect(reverse('index'))
+
+    return render(request,"bank/register.html")
+
